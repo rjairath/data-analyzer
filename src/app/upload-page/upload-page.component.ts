593abd7823901b4c6b10ff76237778bb786cec29
rishabh jairath <rishabhjairath@gmail.com>
2019-06-03 16:57:35 +0530

    from and to maps added

--- src/app/upload-page/upload-page.component.ts
+++ src/app/upload-page/upload-page.component.ts
@@ -23,7 +23,7 @@ export class UploadPageComponent implements OnInit {
   checkbox = [
     {label: "starting location", checked: false},
     {label: "ending location", checked: false}
-  ]
+  ];
 
   constructor() {}
 
@@ -33,9 +33,6 @@ export class UploadPageComponent implements OnInit {
     //Add an extension check
     //Enable the checkbox buttons here
     this.extensionError = false;
-    this.checkbox.forEach((x)=>{
-      x.checked = false;
-    });
     const ele1 = document.getElementById("checkbox1") as HTMLInputElement;
     const ele2 = document.getElementById("checkbox2") as HTMLInputElement;
     ele1.checked = false;
@@ -123,9 +120,21 @@ export class UploadPageComponent implements OnInit {
       this.geoJSONArray2.push(obj);
     }
     // debugger;
-    this.renderMap();
+    // this.renderMap();
   }
-  renderMap(){
+  checkSelected(label: string){
+    const ele1 = document.getElementById("checkbox1") as HTMLInputElement;
+    const ele2 = document.getElementById("checkbox2") as HTMLInputElement;
+    if(label == "starting location"){
+      ele2.checked = false;
+      this.renderMap1();
+    }
+    else{
+      ele1.checked = false;
+      this.renderMap2();
+    }
+  }
+  renderMap1(){
     mapboxgl.accessToken = MAP_ACCESS_TOKEN;
     let temp1 = this.geoJSONArray1;
 
@@ -136,6 +145,7 @@ export class UploadPageComponent implements OnInit {
       zoom: 11
     });
     map.on('load', function() {
+      //Add Source 1
       map.addSource('fromPoints', {
         type: 'geojson',
         data: {
@@ -240,11 +250,120 @@ export class UploadPageComponent implements OnInit {
         .addTo(map);
     });
   }
-  checkSelected(label: string){
-    this.checkbox.forEach((element)=>{
-      if(label == element.label){
-        element.checked = true;
-      }
+  renderMap2(){
+    mapboxgl.accessToken = MAP_ACCESS_TOKEN;
+    let temp2 = this.geoJSONArray2;
+
+    var map = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/mapbox/dark-v10',
+      center: [77.67229, 12.92415],
+      zoom: 11
+    });
+    map.on('load', function() {
+      //Add Source 1
+      map.addSource('toPoints', {
+        type: 'geojson',
+        data: {
+          "type": "FeatureCollection",
+          "features":  temp2
+        }
+      });
+      // add heatmap layer here
+      map.addLayer({
+        id: 'toPoints-heat',
+        type: 'heatmap',
+        source: 'toPoints',
+        maxzoom: 15,
+        paint: {
+          // increase weight as diameter breast height increases
+          'heatmap-weight': {
+            property: 'travelType',
+            type: 'exponential',
+            stops: [
+              [1, 0],
+              [3, 1]
+            ]
+          },
+          // increase intensity as zoom level increases
+          'heatmap-intensity': {
+            stops: [
+              [11, 1],
+              [15, 3]
+            ]
+          },
+          // assign color values be applied to points depending on their density
+          'heatmap-color': [
+            'interpolate',
+            ['linear'],
+            ['heatmap-density'],
+            0, 'rgba(236,222,239,0)',
+            0.2, 'rgb(178,226,226)',
+            0.4, 'rgb(102,194,164)',
+            0.6, 'rgb(44,162,95)',
+            0.8, 'rgb(0,109,44)'
+          ],
+          // increase radius as zoom increases
+          'heatmap-radius': {
+            stops: [
+              [11, 15],
+              [15, 20]
+            ]
+          },
+          // decrease opacity to transition into the circle layer
+          'heatmap-opacity': {
+            default: 1,
+            stops: [
+              [14, 1],
+              [15, 0]
+            ]
+          },
+        }
+      }, 'waterway-label');
+      // add circle layer here
+      map.addLayer({
+        id: 'toPoints-point',
+        type: 'circle',
+        source: 'toPoints',
+        minzoom: 14,
+        paint: {
+          // increase the radius of the circle as the zoom level and travelType value increases
+          'circle-radius': {
+            property: 'travelType',
+            type: 'exponential',
+            stops: [
+              [{ zoom: 15, value: 1 }, 5],
+              [{ zoom: 15, value: 3 }, 10],
+              [{ zoom: 22, value: 1 }, 20],
+              [{ zoom: 22, value: 3 }, 40],
+            ]
+          },
+          'circle-color': {
+            property: 'travelType',
+            type: 'exponential',
+            stops: [
+              [1, 'rgb(236,222,239)'],
+              [2, 'rgb(208,209,230)'],
+              [3, 'rgb(166,189,219)'],
+            ]
+          },
+          'circle-stroke-color': 'white',
+          'circle-stroke-width': 1,
+          'circle-opacity': {
+            stops: [
+              [14, 0],
+              [15, 1]
+            ]
+          }
+        }
+      }, 'waterway-label');
+    });
+
+    map.on('click', 'toPoints-point', function(e) {
+      new mapboxgl.Popup()
+        .setLngLat(e.features[0].geometry.coordinates)
+        .setHTML('<b>travel_type:</b> ' + e.features[0].properties.travelType)
+        .addTo(map);
     });
   }
 }
